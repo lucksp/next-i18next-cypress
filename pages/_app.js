83dc280419fe68516960752b57b591778a55e45c
Phil Lucks <plucks@fluidtruck.com>
2021-04-08 16:53:34 -0600

    test with redirect & app getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import { appWithTranslation } from "next-i18next";
+import Cookie from "js-cookie";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps({ ctx });
+  }
+  let userDate;
+  if (Cookie.get("userDate")) {
+    userDate = null;
+  } else {
+    Cookie.set("userDate", new Date().toISOString());
+    userDate = Cookie.get("userDate");
+  }
+
+  pageProps.userDate = userDate;
+
+  return { pageProps };
+};
 
 export default appWithTranslation(MyApp);
